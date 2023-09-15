753752ab11cad8a6bf51e1a890beca164a55eca3
Israel Vidal <73002064+IsraelVidalParedes@users.noreply.github.com>
2023-09-15 08:21:05 -0600

    functional fruit-only version

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,3 @@
-
 import cv2
 import numpy as np
 import random
@@ -6,6 +5,7 @@ import random
 # Global variables
 prev_frame = None
 fruits = []
+score = 0
 
 class Fruit:
     def __init__(self, x, y, dy):
@@ -25,14 +25,14 @@ def detect_motion(frame, prev_frame):
     if prev_frame is None:
         return None
 
-    # Compute the absolute difference between the current frame and previous frame
     diff = cv2.absdiff(prev_frame, frame)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 25, 255, cv2.THRESH_BINARY)
-    return thresh
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
 
 def main():
-    global prev_frame, fruits
+    global prev_frame, fruits, score
 
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
@@ -44,28 +44,27 @@ def main():
         if not ret:
             print("Error: Can't receive frame. Exiting ...")
             break
-        
 
         frame = cv2.flip(frame, 1)
-        motion_mask = detect_motion(frame, prev_frame)
+        contours = detect_motion(frame, prev_frame)
         prev_frame = frame.copy()
 
-        # Add fruits
         if random.random() < 0.02:
             fruits.append(Fruit(random.randint(0, frame.shape[1]), 0, random.randint(5, 10)))
 
-        # Update and draw fruits
         for fruit in fruits:
             fruit.update()
             fruit.draw(frame)
 
-            # Check for slicing
-            if motion_mask is not None:
-                if motion_mask[int(fruit.y), int(fruit.x)] == 255:
-                    fruit.sliced = True
+            if contours:
+                for contour in contours:
+                    if cv2.pointPolygonTest(contour, (fruit.x, fruit.y), False) > 0:
+                        fruit.sliced = True
+                        score += 1
 
-        # Remove sliced and out-of-bound fruits
         fruits = [fruit for fruit in fruits if not fruit.sliced and fruit.y < frame.shape[0]]
+
+        cv2.putText(frame, f"Score: {score}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         cv2.imshow('Fruit Ninja', frame)
 
