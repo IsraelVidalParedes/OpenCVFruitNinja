7be3bcaf6b55d60706b09cb8a88612ea96b5d782
Israel Vidal <73002064+IsraelVidalParedes@users.noreply.github.com>
2023-09-15 08:26:00 -0600

    functional bombs

--- proyecto.py
+++ proyecto.py
@@ -5,9 +5,11 @@ import random
 # Global variables
 prev_frame = None
 fruits = []
+bombs = []
 score = 0
+lives = 3
 
-class Fruit:
+class Entity:
     def __init__(self, x, y, dy):
         self.x = x
         self.y = y
@@ -17,9 +19,15 @@ class Fruit:
     def update(self):
         self.y += self.dy
 
+class Fruit(Entity):
     def draw(self, frame):
         if not self.sliced:
             cv2.circle(frame, (self.x, self.y), 20, (0, 255, 0), -1)
+
+class Bomb(Entity):
+    def draw(self, frame):
+        if not self.sliced:
+            cv2.circle(frame, (self.x, self.y), 20, (0, 0, 255), -1)
 
 def detect_motion(frame, prev_frame):
     if prev_frame is None:
@@ -32,7 +40,7 @@ def detect_motion(frame, prev_frame):
     return contours
 
 def main():
-    global prev_frame, fruits, score
+    global prev_frame, fruits, bombs, score, lives
 
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
@@ -51,20 +59,33 @@ def main():
 
         if random.random() < 0.02:
             fruits.append(Fruit(random.randint(0, frame.shape[1]), 0, random.randint(5, 10)))
+        if random.random() < 0.01:  # Bombs are less frequent
+            bombs.append(Bomb(random.randint(0, frame.shape[1]), 0, random.randint(5, 10)))
 
-        for fruit in fruits:
-            fruit.update()
-            fruit.draw(frame)
+        entities = fruits + bombs
+        for entity in entities:
+            entity.update()
+            entity.draw(frame)
 
             if contours:
                 for contour in contours:
-                    if cv2.pointPolygonTest(contour, (fruit.x, fruit.y), False) > 0:
-                        fruit.sliced = True
-                        score += 1
+                    if cv2.pointPolygonTest(contour, (entity.x, entity.y), False) > 0:
+                        entity.sliced = True
+                        if isinstance(entity, Fruit):
+                            score += 1
+                        elif isinstance(entity, Bomb):
+                            lives -= 1
+                            if lives <= 0:
+                                print("Game Over!")
+                                cap.release()
+                                cv2.destroyAllWindows()
+                                return
 
         fruits = [fruit for fruit in fruits if not fruit.sliced and fruit.y < frame.shape[0]]
+        bombs = [bomb for bomb in bombs if not bomb.sliced and bomb.y < frame.shape[0]]
 
         cv2.putText(frame, f"Score: {score}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(frame, f"Lives: {lives}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         cv2.imshow('Fruit Ninja', frame)
 
